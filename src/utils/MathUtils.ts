9376cfad822b5c2a6949ebb0fc22f87f2d7c7744
rico <rico@cg.app>
2021-08-30 11:56:57 +0800

    feat: 优化numberFormat方法

--- src/utils/MathUtils.ts
+++ src/utils/MathUtils.ts
@@ -1,16 +1,26 @@
-export default class MathUtils {
-  static scientificNotationToString(number: number): string {
-    const strParam = String(number).replace(/^-/, '')
-    if (!/e/.test(strParam)) return strParam
+interface numberFormatOptions {
+  precision?: number //小数点位数
+  zeroPadding?: boolean //是否进行补领操作
+  mode?: 'round' | 'ceil' | 'floor' | 'cut-off' //处理模式，四舍五入，入，舍
+  symbol?: string //金额标识，传$等货币符号可拼接在前，默认为空
+  separator?: string //分割符号，按groups规则对整数位进行分割，默
+  decimal?: string //小数点的符号，默认英文'.'
+  groups?: RegExp //整数位分割规则，默认千分位，每三位用separator符号进行分割
+}
 
+export default class MathUtils {
+  static toString(number: number): string {
+    const string = String(number)
+    if (!/e/.test(string)) return string
+    const strAbs = string.replace(/^-/, '')
     // 指数
-    const index = Number(strParam.match(/\d+$/)?.[0])
+    const index = Number(strAbs.match(/\d+$/)?.[0])
     // 基数
-    const basis = strParam.match(/^[\d\.]+/)?.[0].replace(/\./, '') || ''
+    const basis = strAbs.match(/^[\d\.]+/)?.[0].replace(/\./, '') || ''
 
     return (number >= 0 ? '#' : '-#').replace(
       '#',
-      /e\+/.test(strParam)
+      /e\+/.test(strAbs)
         ? basis.padEnd(index + 1, '0')
         : basis.padStart(index + basis.length, '0').replace(/^0/, '0.')
     )
@@ -18,26 +28,18 @@ export default class MathUtils {
 
   static numberFormat(
     number: number,
-    options: {
-      precision?: number //小数点位数
-      zeroPadding?: boolean //是否进行补领操作
-      mode?: 'round' | 'ceil' | 'floor' | 'cut-off' //处理模式，四舍五入，入，舍
-      symbol?: string //金额标识，传$等货币符号可拼接在前，默认为空
-      separator?: string //分割符号，按groups规则对整数位进行分割，默
-      decimal?: string //小数点的符号，默认英文'.'
-      groups?: RegExp //整数位分割规则，默认千分位，每三位用separator符号进行分割
-    } = {}
+    options: numberFormatOptions = {}
   ): string {
     const { precision, mode, symbol, decimal, groups, separator, zeroPadding } =
       Object.assign(
         {
-          symbol: '',
-          precision: 2,
-          zeroPadding: true,
+          symbol: '', //默认金额符号为空
+          precision: 2, //默认保留2位小数
+          zeroPadding: true, //默认开启补零操作
           mode: 'cut-off', //默认截取模式
-          separator: ',',
-          decimal: '.',
-          groups: /(\d)(?=(\d{3})+\b)/g
+          separator: ',', //默认整数位分隔符为英文逗号
+          decimal: '.', //默认小数点符号为英文句号
+          groups: /(\d)(?=(\d{3})+\b)/g //默认规则为每3位一分组
         },
         options
       )
@@ -48,9 +50,7 @@ export default class MathUtils {
         number * scale
       ) / scale
     //分开处理整数位和小数位
-    const split = this.scientificNotationToString(value)
-        .replace(/^-/, '')
-        .split('.'),
+    const split = this.toString(value).replace(/^-/, '').split('.'),
       dollars = split[0],
       cents = (split[1] || '').padEnd(zeroPadding ? precision : 0, '0')
 
