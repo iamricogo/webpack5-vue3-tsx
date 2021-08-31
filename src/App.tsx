e8f50fa063cdb14530facfc1f76b7fcb932ed96d
rico <iamricogo@gmail.com>
2021-08-31 21:03:44 +0800

    delete: 删除多余代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import { defineComponent } from 'vue'
 import createStore from '@/store/provide'
-interface Geolocation {
-  longitude: number
-  latitude: number
-}
-export interface UpdateProvide {
-  location: (value: string) => void
-  geolocation: (value: Geolocation) => void
-}
 export default defineComponent({
   name: 'App',
   setup: () => {
