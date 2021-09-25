8ebf6402430b569603c660c9bdf764dcbb167e87
rico <iamricogo@gmail.com>
2021-09-25 13:29:26 +0800

    feat: .vue 文件 demo 中更新setup中取上下文的例子

--- src/views/others/index.vue
+++ src/views/others/index.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="others-pages">
-    <tag>others</tag>
-
-    <p>state.deep.persisted:{{ store.state.deep.persisted }}</p>
-    <p>state.deep.normal:{{ store.state.deep.normal }}</p>
-
+    <tag>自建状态管理</tag>
+    <p>store.state.deep.persisted:{{ store.state.deep.persisted }}</p>
+    <p>store.state.deep.normal:{{ store.state.deep.normal }}</p>
     <button @click="plus">+</button>
     <button @click="minus">-</button>
+    <br />
+    <tag>vuex状态管理</tag>
+    <p>$store.state.app.language:{{ $store.state.app.language }}</p>
   </div>
 </template>
 <script lang="ts">
 import { Tag } from 'ant-design-vue'
-import { defineComponent } from 'vue'
+import { defineComponent, getCurrentInstance } from 'vue'
 import { useStore } from '@/store/hooks'
-
+let context!: unknown
 export default defineComponent({
   name: 'Others',
   components: {
     Tag
   },
   setup: () => {
+    const instance = getCurrentInstance()
+    context = instance?.proxy
     const store = useStore()
     return { store }
   },
   methods: {
     plus() {
+      console.log(this)
+      console.log(this === context)
       const {
         state: {
           deep: { persisted, normal }
