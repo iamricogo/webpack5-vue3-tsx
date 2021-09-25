7e7f505a9310bf49cafdc2423157f00b8482409d
rico <iamricogo@gmail.com>
2021-09-25 13:38:22 +0800

    feat: vue模板中添加用render函数写demo

--- src/views/others/index.vue
+++ src/views/others/index.vue
@@ -10,7 +10,7 @@
     <p>$store.state.app.language:{{ $store.state.app.language }}</p>
   </div>
 </template>
-<script lang="ts">
+<script lang="tsx">
 import { Tag } from 'ant-design-vue'
 import { defineComponent, getCurrentInstance } from 'vue'
 import { useStore } from '@/store/hooks'
@@ -26,6 +26,23 @@ export default defineComponent({
     const store = useStore()
     return { store }
   },
+  /**
+   * 不写template 也可以 配合 render 函数写结构render 函数里面可以用 options api 也可以用 组合式 api
+   */
+  // render() {
+  //   return (
+  //     <div class="others-pages">
+  //       <tag>自建状态管理</tag>
+  //       <p>store.state.deep.persisted:{this.store.state.deep.persisted}</p>
+  //       <p>store.state.deep.normal:{this.store.state.deep.normal}</p>
+  //       <button onClick={this.plus}>+</button>
+  //       <button onClick={this.minus}>-</button>
+  //       <br />
+  //       <tag>vuex状态管理</tag>
+  //       <p>$store.state.app.language:{this.$store.state.app.language}</p>
+  //     </div>
+  //   )
+  // },
   methods: {
     plus() {
       console.log(this)
